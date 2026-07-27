4caeb8378ca9a3041247c6025b6ded5935aa3777
Sahil17102 <Sahil17102@users.noreply.github.com>
2026-07-27 12:24:00 +0530

    Fix Render publish directory

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  distDir: "dist",
   images: {
     formats: ["image/avif", "image/webp"],
     unoptimized: true,
